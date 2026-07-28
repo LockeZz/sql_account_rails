06c4aa5ee59453ca4dea1932454f55b0106f0cd2
LockeZz <LockeZz.PHF@gmail.com>
2026-07-28 13:03:22 +0800

    updated code such that the establish connection is not using active record base

--- lib/sql_account/connection.rb
+++ lib/sql_account/connection.rb
@@ -2,7 +2,8 @@ module SqlAccount
   module Connection
 
     def self.establish!
-      ActiveRecord::Base.establish_connection(SqlAccount.configuration.to_h)
+      # ActiveRecord::Base.establish_connection(SqlAccount.configuration.to_h)
+      SqlAccount::Record.establish_connection(SqlAccount.configuration.to_h)
     end
 
   end
